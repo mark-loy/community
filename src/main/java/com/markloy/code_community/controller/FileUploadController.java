d0fb6adb6a8cecdc32daaacee3079ee11f1fee2b
markloy <mark_loy@163.com>
2020-09-10 21:01:27 +0800

    OSS文件上传

--- src/main/java/com/markloy/code_community/controller/FileUploadController.java
+++ src/main/java/com/markloy/code_community/controller/FileUploadController.java
@@ -1,21 +1,51 @@
 package com.markloy.code_community.controller;
 
 import com.markloy.code_community.dto.FileUploadDTO;
+import com.markloy.code_community.provider.OSSProvider;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URL;
 
 @Controller
+@Log4j2
 public class FileUploadController {
+
+    @Autowired
+    private OSSProvider ossProvider;
 
     @ResponseBody
     @PostMapping("/fileUpload")
-    public FileUploadDTO fileUpload() {
+    public FileUploadDTO fileUpload(HttpServletRequest request) {
+        //转化request对象
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile parameter = multipartRequest.getFile("editormd-image-file");
+        URL url = null;
         FileUploadDTO fileUploadDTO = new FileUploadDTO();
-        fileUploadDTO.setSuccess(1);
-        fileUploadDTO.setMessage("上传成功");
-        fileUploadDTO.setUrl("http://localhost:8080/images/file1.png");
+        try {
+            url = ossProvider.fileUpload(parameter);
+            if (url != null) {
+                fileUploadDTO.setSuccess(1);
+                fileUploadDTO.setMessage("文件上传成功");
+                fileUploadDTO.setUrl(url.toString());
+            } else {
+                fileUploadDTO.setSuccess(0);
+                fileUploadDTO.setMessage("文件上传失败");
+            }
+        } catch (IOException e) {
+            fileUploadDTO.setSuccess(0);
+            fileUploadDTO.setMessage("文件上传失败");
+            log.log(Level.INFO, "文件上传失败" + e);
+        }
         return fileUploadDTO;
     }
 
---
+++ src/main/java/com/markloy/code_community/provider/OSSProvider.java
@@ -0,0 +1,53 @@
+package com.markloy.code_community.provider;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class OSSProvider {
+    @Value("${aliyun.endpoint}")
+    private String  endpoint;
+    @Value("${aliyun.accessKeyId}")
+    private String accessKeyId ;
+    @Value("${aliyun.accessKeySecret}")
+    private String accessKeySecret;
+    @Value("${aliyun.bucketName}")
+    private String bucketName;
+    @Value("${aliyun.filePatten}")
+    private String filePatten;
+
+    /**
+     * 文件上传
+     * @param file 文件
+     * @return OSS外网路径
+     */
+    public URL fileUpload(MultipartFile file) throws IOException {
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        //对文件名进行处理  \\代表转义
+        String[] strings = Objects.requireNonNull(file.getOriginalFilename()).split("\\.");
+        String suffix = strings[strings.length - 1];
+        String fileName = UUID.randomUUID() + "." + suffix;
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, filePatten + fileName, file.getInputStream());
+        ossClient.putObject(putObjectRequest);
+        //过期时间
+        Date expiration = new Date(System.currentTimeMillis() + 60 * 60 * 24 * 30);
+        //获取返回的文件外网路径
+        URL url = ossClient.generatePresignedUrl(bucketName, filePatten + fileName, expiration);
+        ossClient.shutdown();
+        return url;
+    }
+
+
+}
